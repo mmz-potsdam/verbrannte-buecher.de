3cd570a0255211ca3cc69c5675ac834ae7a169c0
Daniel Burckhardt <daniel.burckhardt@sur-gmbh.ch>
2023-02-22 15:58:54 +0100

    composer require presta/sitemap-bundle

--- config/bundles.php
+++ config/bundles.php
@@ -3,4 +3,5 @@
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
+    Presta\SitemapBundle\PrestaSitemapBundle::class => ['all' => true],
 ];
