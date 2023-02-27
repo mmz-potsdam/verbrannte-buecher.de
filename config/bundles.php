307d697fe91f19570808e6c88b5e712b3835719c
Daniel Burckhardt <daniel.burckhardt@sur-gmbh.ch>
2023-02-27 07:56:12 +0100

    Add liip/url-auto-converter-bundle

--- config/bundles.php
+++ config/bundles.php
@@ -4,4 +4,5 @@ return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Presta\SitemapBundle\PrestaSitemapBundle::class => ['all' => true],
+    Liip\UrlAutoConverterBundle\LiipUrlAutoConverterBundle::class => ['all' => true],
 ];
