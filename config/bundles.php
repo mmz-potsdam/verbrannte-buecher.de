f40cddf6f24a9eb6967202dfc173b7029cc77565
Daniel Burckhardt <daniel.burckhardt@sur-gmbh.ch>
2023-03-29 16:18:17 +0200

    composer require symfony/security-bundle see https://symfony.com/doc/5.4/security.html

--- config/bundles.php
+++ config/bundles.php
@@ -20,4 +20,5 @@ return [
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Armin\ScssphpBundle\ScssphpBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
+    Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
 ];
